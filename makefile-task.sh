1c0839010ecdafa307e6dcab8828f54b9bfea4d0
Francesco Ciocchetti <fciocchetti@mintel.com>
2019-01-24 11:20:42 +0000

    Make it just run make

--- makefile-task.sh
+++ makefile-task.sh
@@ -3,10 +3,7 @@ set -e
 
 task=$1
 
-file=$2
-
 [[ -z $task ]] && echo "Missing TASK argument" && exit 1
-[[ -z $file ]] && echo "Missing Makefile argument" && exit 1
 
-make --file=$2 $1
+make --file=$2
 
