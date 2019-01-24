53cf6249eb3ed78b09233950d80f9704d0f17f53
Francesco Ciocchetti <fciocchetti@mintel.com>
2019-01-24 11:32:51 +0000

    Run the right task

--- makefile-task.sh
+++ makefile-task.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+## NO CHEKCS OF ANYTHING , this is probably dagenereous and exploitable - make sure you know what you are doing by using this
+
 task=$1
 
 [[ -z $task ]] && echo "Missing TASK argument" && exit 1
 
-make --file=$2
+make $1
 
